437fe63fb0ad877750b87fb7eb6dbd2631f4b070
mrsharp-milken <144956668+mrsharp-milken@users.noreply.github.com>
2024-12-05 17:49:50 -0800

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -54,23 +54,23 @@ def run_tests():
     expected_data = {'Evermore': [1, 1, 1, 1, 1, 1, 1], 'Vanguard City': [1, 2, 3, 4, 5, 6, 7], 'Excelsior': [1, 1, 2, 3, 5, 8, 13]}
     actual_data = load_data('data/disease1.txt')
     test1 = (actual_data == expected_data)
-    print(f"Test 1 passed: {test1}")
+    print(f"Test 1 (load_data) passed: {test1}")
 
     # Test 2: daily_cases (Test 1)
     test2_input = {'Test': [1, 2, 3, 4, 4, 4, 4]}
     expected_output2 = {'Test': [1, 1, 1, 1, 0, 0, 0]}
     actual_output2 = daily_cases(test2_input)
     test2 = (actual_output2 == expected_output2)
-    print(f"Test 2 passed: {test2}")
+    print(f"Test 2 (daily_cases) passed: {test2}")
 
     # Test 3: daily_cases (Test 2)
     test3_input = { 'Evermore': [1, 1, 1, 1, 1, 1, 1], 'Vanguard City': [1, 2, 3, 4, 5, 6, 7], 'Excelsior': [1, 1, 2, 3, 5, 8, 13] }
     expected_output3 = { 'Evermore': [1, 0, 0, 0, 0, 0, 0], 'Vanguard City': [1, 1, 1, 1, 1, 1, 1], 'Excelsior': [1, 0, 1, 1, 2, 3, 5] }
     actual_output3 = daily_cases(test3_input)
     test3 = (actual_output3 == expected_output3)
-    print(f"Test 3 passed: {test3}")
+    print(f"Test 3 (daily_cases) passed: {test3}")
 
-    print("Remember, these are just basic tests! Make sure to try other files")
+    print("Remember, these are just basic tests! Make sure to try other data files")
 
 
 def main():
